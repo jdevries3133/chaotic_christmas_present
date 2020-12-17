408f1c79bf62b3f04be782c23436c28fc163b1de
JohnDeVries <jdevries3133@gmail.com>
2020-12-16 22:38:04 -0500

    sql injection attack succeeds

--- thomasdevri_es/sql_vulnerable/tests.py
+++ thomasdevri_es/sql_vulnerable/tests.py
@@ -8,33 +8,45 @@ from .models import ContactFormData
 # Create your tests here.
 class TestFormVulnerability(TestCase):
 
+    def setUp(self):
+        for _ in range(10):
+            ContactFormData.objects.create(
+                subject='setup subject',
+                message='setup message',
+            )
+
     def test_form_executes_query(self):
         client = Client()
         with self.assertNumQueries(1):
             client.post('/contact/', {
-                'subject': 'any',
-                'message': 'SELECT name from SQLITE_MASTER;',
+                'subject': 'foo',
+                'message': 'bar',
             })
 
     def test_form_works_with_normal_use(self):
-        """
-        Naive "expected" behavior is for database insertion to happen
-        unconsequentially if an SQL injection attack is not attempted.
-        """
         client = Client()
         client.post('/contact/', {
             'subject': 'foo',
             'message': 'bar',
         })
-        self.assertTrue(ContactFormData.objects.filter(subject='foo', message='bar'))
+        self.assertTrue(ContactFormData.objects.filter(subject='foo', message='bar'))  # type: ignore
 
     def test_custom_query_mimics_orm(self):
         with CaptureQueriesContext(connection) as ctx:
-            orm_query = 'INSERT INTO "sql_vulnerable_contactformdata" ("subject", "message") VALUES (\'a\', \'b\')'
             client = Client()
             client.post('/contact/', {
                 'subject': 'foo',
                 'message': 'bar',
             })
-            query = "INSERT INTO sql_vulnerable_contactformdata (subject, message) VALUES ('foo', 'bar');"
-            self.assertTrue(query in [i['sql'] for i in ctx.captured_queries])
+            orm_query = "INSERT INTO sql_vulnerable_contactformdata (subject, message) VALUES ('foo', 'bar')"
+            self.assertTrue(orm_query in [i['sql'] for i in ctx.captured_queries])
+
+    def test_sql_injection_successful(self):
+        client = Client()
+        injected_query = 'SELECT * FROM sql_vulnerable_contactformdata'
+        with CaptureQueriesContext(connection) as ctx:
+            client.post('/contact/', {
+                'subject': 'foo',
+                'message': 'bar\'); SELECT * FROM sql_vulnerable_contactformdata; ',
+            })
+            self.assertTrue(injected_query in [i['sql'].strip() for i in ctx.captured_queries])
--- thomasdevri_es/sql_vulnerable/views.py
+++ thomasdevri_es/sql_vulnerable/views.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.shortcuts import render
 from django.db import connection
+from django.db.utils import OperationalError
 
-from sql_vulnerable.models import ContactFormData
+from sql_vulnerable.models import ContactFormData  # type: ignore
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'sql_vulnerable/home.html')
@@ -14,5 +19,14 @@ def formpage(request):
                 f'(subject, message) VALUES (\'{request.POST["subject"]}\', '
                 f'\'{request.POST["message"]}\');'
             )
-            res = cursor.execute(query)
+            queries = [q for q in query.split(';') if q]
+            all_responses = []
+            for query in queries:
+                try:
+                    res = cursor.execute(query)
+                    all_responses += res.fetchall()
+                except OperationalError:
+                    # invalid sql is expected; it will come from the remnants
+                    # after the escaped sql.
+                    logger.debug(f'Invalid sql: {query}')
     return render(request, 'sql_vulnerable/formpage.html')
