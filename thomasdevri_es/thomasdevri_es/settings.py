e9a39269a757cd692be41e96658d11c95b47dab2
JohnDeVries <jdevries3133@gmail.com>
2020-12-25 05:36:19 -0500

    configured logging

--- thomasdevri_es/thomasdevri_es/settings.py
+++ thomasdevri_es/thomasdevri_es/settings.py
@@ -127,19 +127,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-# LOGGING = {
-#     'version': 1,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'root': {
-#         'handlers': ['console'],
-#         'level': 'DEBUG',
-#     },
-#     'django': {
-#         'handlers': ['console'],
-#         'level': 'INFO',
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'thomasdevri.es.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
