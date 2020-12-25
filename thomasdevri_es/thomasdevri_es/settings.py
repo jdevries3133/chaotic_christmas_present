cdd3e4bb40901d80334168988a7195ffc52dea1f
Jack DeVries <jdevries3133@gmail.com>
2020-12-25 10:33:25 +0000

    remote changes and configuration

--- thomasdevri_es/thomasdevri_es/settings.py
+++ thomasdevri_es/thomasdevri_es/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-# doesn't work, idk why
 # LOGGING = {
 #     'version': 1,
 #     'handlers': {
